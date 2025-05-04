2f8df41178b2fee196e6e0a6bff83f77fac3d3a0
repeat98 <jannik.assfalg@gmail.com>
2025-05-04 22:46:09 +0200

    Update project configuration for deployment and add new dependencies. Changed output directory from 'dist' to 'docs' in vite.config.ts, added 'gh-pages' for deployment in package.json, and updated package-lock.json with new dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import tsconfigPaths from 'vite-plugin-tsconfig-paths';
 export default defineConfig({
   // point Vite at your UI folder
   root: 'src/ui',
+  base: '/crypto-tick-rsi/',
 
   // so imports like `import { ChartManager } from '@/charts/chartManager'` still work
   plugins: [tsconfigPaths()],
 
   // where to put the built files
   build: {
-    outDir: '../../dist',
+    outDir: '../../docs',
     emptyOutDir: true,
   },
 });
